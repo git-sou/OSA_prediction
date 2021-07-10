fbd717105f630e305fa4fefd44df1133febd6270
samy-oussaidene <83417933+samy-oussaidene@users.noreply.github.com>
2021-07-10 15:11:27 +0200

    Add files via upload

---
+++ draft.R
@@ -0,0 +1,14 @@
+########################################
+########################################
+####   NEXT STEPS:
+####
+####  You can try:
+####        - Regularization
+####        - Feature selection
+####        - other Regression models
+####
+####  BUT you can wait to see Classification Script
+####           and use regression with some libraries as CARET
+#### Comparison of Linear Regression with K Nearest Neighbors
+
+
